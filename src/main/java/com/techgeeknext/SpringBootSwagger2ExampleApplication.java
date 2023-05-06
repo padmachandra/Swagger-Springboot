a643bf4d500621c393905c94055b8122089d43d9
dp <dp@vsphere.lab>
2023-05-06 10:59:26 +0530

    swagger code

---
+++ src/main/java/com/techgeeknext/SpringBootSwagger2ExampleApplication.java
@@ -0,0 +1,15 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwagger2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2ExampleApplication.class, args);
+	}
+}
+
+
+//test with http://localhost:8080/swagger-ui.html#/
